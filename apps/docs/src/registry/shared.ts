826bcbd69bd6f1acbbbc43359b74136479111e09
Maged Ibrahim <magedibra@yahoo.com>
2026-03-18 18:18:36 +0200

    feat: add warning and success colors

--- apps/docs/src/registry/shared.ts
+++ apps/docs/src/registry/shared.ts
@@ -7,16 +7,26 @@ export const cssVars = {
     "--default": "oklch(94% 0.001 286.375)",
     "--default-foreground": "var(--eclipse)",
     "--destructive-foreground": "var(--snow)",
+    "--success": "oklch(0.7329 0.1935 150.81)",
+    "--success-foreground": "var(--eclipse)",
+    "--warning": "oklch(0.7819 0.1585 72.33)",
+    "--warning-foreground": "var(--eclipse)",
   },
   dark: {
     "--default": "oklch(27.4% 0.006 286.033)",
     "--default-foreground": "var(--snow)",
     "--destructive-foreground": "var(--snow)",
+    "--warning": "oklch(0.8203 0.1388 76.34)",
+    "--warning-foreground": "var(--eclipse)",
   },
   theme: {
     "--color-default": "var(--default)",
     "--color-default-foreground": "var(--default-foreground)",
     "--color-destructive-foreground": "var(--destructive-foreground)",
+    "--color-success": "var(--success)",
+    "--color-success-foreground": "var(--success-foreground)",
+    "--color-warning": "var(--warning)",
+    "--color-warning-foreground": "var(--warning-foreground)",
   },
 };
 export const css = {
