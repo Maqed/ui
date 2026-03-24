17a61726182e06426b94543b35e09456171a613b
Maged Ibrahim <magedibra@yahoo.com>
2026-03-24 17:28:59 +0200

    feat: improve hero section

--- apps/docs/src/app/(home)/page.tsx
+++ apps/docs/src/app/(home)/page.tsx
@@ -1,16 +1,25 @@
 import Link from "next/link";
+import { Button } from "@/registry/new-york-v4/ui/button";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col justify-center text-center flex-1">
+    <div className="flex flex-col justify-center text-center py-4">
       <h1 className="text-4xl font-bold mb-4">
-        Build your next styled shadcn/ui components
+        <span>HeroUI's design system.</span>
+        <br />
+        <span className="text-muted-foreground">shadcn's abstraction.</span>
       </h1>
-      <p>
-        <Link href="/docs" className="font-medium text-xl underline">
-          /docs
-        </Link>
-      </p>
+      <div className="flex justify-center items-center gap-3">
+        <Button
+          nativeButton={false}
+          render={<Link href="/docs/installation">Install</Link>}
+        />
+        <Button
+          nativeButton={false}
+          variant="secondary"
+          render={<Link href="/docs/components">View components</Link>}
+        />
+      </div>
     </div>
   );
 }
