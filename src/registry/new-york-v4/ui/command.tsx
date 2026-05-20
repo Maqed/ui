9d8dc61edb0b9f8eaa127b103605eb5b6363bebd
Maged Ibrahim <magedibra@yahoo.com>
2026-05-20 17:09:26 +0300

    chore: improve code

--- src/registry/new-york-v4/ui/command.tsx
+++ src/registry/new-york-v4/ui/command.tsx
@@ -161,7 +161,7 @@ function CommandItem({
 		<CommandPrimitive.Item
 			data-slot="command-item"
 			className={cn(
-				"group/command-item relative flex cursor-pointer select-none items-center gap-2 rounded-lg px-3 py-2 text-sm outline-hidden data-[disabled=true]:pointer-events-none data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0 data-[selected=true]:*:[svg]:text-foreground",
+				"group/command-item relative flex cursor-pointer select-none items-center gap-2 rounded-xl px-3 py-2.5 text-sm outline-hidden data-[disabled=true]:pointer-events-none data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0 data-[selected=true]:*:[svg]:text-foreground",
 				className,
 			)}
 			{...props}
